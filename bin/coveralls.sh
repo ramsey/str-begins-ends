4cd33dbfccd7686baf0cc8fc80fd4f0ef96f6f4c
Ben Ramsey <ben@benramsey.com>
2019-06-18 19:12:37 -0500

    Fix issue with travis_retry command

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-[[ -f vendor/bin/php-coveralls ]] && travis_retry php vendor/bin/php-coveralls -v || travis_retry php vendor/bin/coveralls -v
+[[ -f vendor/bin/php-coveralls ]] && php vendor/bin/php-coveralls -v || php vendor/bin/coveralls -v
